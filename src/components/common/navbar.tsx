74845f7c1551adbd11075a48b3985869b1208684
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-05-28 19:54:00 +0700

    fix: navbar layout

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -3,14 +3,43 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from './menubar';
 import { PaddingContainer } from './paddingContainer';
 import Image from 'next/image';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@/lib/redux-toolkit/store';
+import { deleteCookie } from '@/app/action';
+import { resetUserInformation } from '@/lib/redux-toolkit/user-information/userInformation';
+import { Button } from './button';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/service/firebase';
 
 export function Navbar() {
+  const dispatch = useDispatch();
+  const userInformation = useSelector(
+    (state: RootState) => state.userInformation,
+  );
+
+  const handleLogout = async () => {
+    try {
+      const result = await signOut(auth);
+      console.log(result);
+    } catch (error) {
+      console.error('Error signing out: ', error);
+    }
+  };
+
+  const clearInformation = async () => {
+    try {
+      const clearCookie = await deleteCookie();
+      const logout = await handleLogout();
+      dispatch(resetUserInformation());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <header>
       <Menubar className="h-auto">
@@ -18,17 +47,25 @@ export function Navbar() {
           <div className="flex justify-between items-center py-[3px]">
             <h1 className="text-2xl font-semibold">Notify</h1>
             <MenubarMenu>
-              <MenubarTrigger>
+              <MenubarTrigger className="cursor-pointer">
                 <Image
-                  src={'https://www.robohash.org/ll?set=set3'}
+                  className="h-[40px] w-[40px] rounded-full"
+                  src={
+                    userInformation.photoUrl !== '' ||
+                    userInformation.photoUrl !== null
+                      ? userInformation.photoUrl
+                      : `https://www.robohash.org/${userInformation.displayName}?set=set3`
+                  }
                   alt=""
-                  width={40}
-                  height={40}
+                  width={0}
+                  height={0}
                   sizes="100vw"
                 />
               </MenubarTrigger>
-              <MenubarContent>
-                <MenubarItem>Logout</MenubarItem>
+              <MenubarContent className="relative top-[20px] flex flex -col justify-center right-[100px]">
+                <MenubarItem className="">
+                  <Button onClick={clearInformation}>Logout</Button>
+                </MenubarItem>
               </MenubarContent>
             </MenubarMenu>
           </div>
--- src/components/notePage/notePage.tsx
+++ src/components/notePage/notePage.tsx
@@ -1,38 +1,14 @@
 'use client';
 import { PaddingContainer, Button, Navbar } from '../common';
-import { auth } from '@/service/firebase';
-import { signOut } from 'firebase/auth';
-import { useDispatch } from 'react-redux';
-import { resetUserInformation } from '@/lib/redux-toolkit/user-information/userInformation';
-import { deleteCookie } from '@/app/action';
 
 export default function NotePage() {
-  const dispatch = useDispatch();
-
-  const handleLogout = async () => {
-    try {
-      const result = await signOut(auth);
-      console.log(result);
-    } catch (error) {
-      console.error('Error signing out: ', error);
-    }
-  };
-
-  const clearInformation = async () => {
-    try {
-      const clearCookie = await deleteCookie();
-      const logout = await handleLogout();
-      dispatch(resetUserInformation());
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <>
       <Navbar />
       <PaddingContainer>
-        <h1>Haloo</h1>
-        <Button onClick={clearInformation}>Logout</Button>
+        <div className="mt-[10px]">
+          <h1>Haloo</h1>
+        </div>
       </PaddingContainer>
     </>
   );
