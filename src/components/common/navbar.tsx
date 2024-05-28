c3b66999cd8c1539f10ad90766d9d5f4bd6e4b73
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-05-28 14:55:28 +0700

    feat: add navbar component

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -8,16 +8,25 @@ import {
   MenubarTrigger,
 } from './menubar';
 import { PaddingContainer } from './paddingContainer';
+import Image from 'next/image';
 
 export function Navbar() {
   return (
     <header>
       <Menubar className="h-auto">
         <PaddingContainer>
-          <div className="flex justify-between items-center py-[10px]">
+          <div className="flex justify-between items-center py-[3px]">
             <h1 className="text-2xl font-semibold">Notify</h1>
             <MenubarMenu>
-              <MenubarTrigger>File</MenubarTrigger>
+              <MenubarTrigger>
+                <Image
+                  src={'https://www.robohash.org/ll?set=set3'}
+                  alt=""
+                  width={40}
+                  height={40}
+                  sizes="100vw"
+                />
+              </MenubarTrigger>
               <MenubarContent>
                 <MenubarItem>Logout</MenubarItem>
               </MenubarContent>
