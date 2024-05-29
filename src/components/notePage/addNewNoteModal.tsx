1282fdfc1028341bb7b3143921355c30c279e7c3
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-05-29 18:28:52 +0700

    fix: z index add note icon

--- src/components/notePage/addNewNoteModal.tsx
+++ src/components/notePage/addNewNoteModal.tsx
@@ -15,7 +15,7 @@ export function AddNewNoteModal() {
   return (
     <Dialog>
       <DialogTrigger>
-        <Button className="fixed right-[100px] bottom-[30px] flex justify-center items-center h-[50px] w-[50px] p-0 rounded-full transition-[0.3s] active:scale-[0.96]">
+        <Button className="z-20 fixed right-[100px] bottom-[30px] flex justify-center items-center h-[50px] w-[50px] p-0 rounded-full transition-[0.3s] active:scale-[0.96]">
           <Plus size={30} />
         </Button>
       </DialogTrigger>
