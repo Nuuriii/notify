c31676e6d261f9bd58790541d72add1736b58f8f
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-06-04 19:07:24 +0700

    feat: add edit note function

--- src/app/api/note/[id]/route.ts
+++ src/app/api/note/[id]/route.ts
@@ -1,13 +1,36 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { database } from '@/service/firebase';
-import { doc, deleteDoc } from 'firebase/firestore';
+import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const noteId = params.id;
+    const body = await req.json();
+    const { title, content } = body;
+
+    const noteRef = doc(database, 'notes', noteId);
+    const updatedTodo = await updateDoc(noteRef, {
+      title,
+      content,
+      updatedAt: new Date(),
+    });
+
+    return NextResponse.json({
+      status: 'success',
+      message: 'success updated note',
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
 
 export async function DELETE(
   req: NextRequest,
   { params }: { params: { id: string } },
 ) {
-  // console.log(params.id);
-  // return NextResponse.json({ message: 'hello' });
   try {
     const id = params.id;
     const noteRef = doc(database, 'notes', id);
--- src/components/notePage/editNoteModal.tsx
+++ src/components/notePage/editNoteModal.tsx
@@ -8,10 +8,37 @@ import {
   DialogTrigger,
   Button,
   Input,
+  Textarea,
 } from '../common';
 import { FilePen } from 'lucide-react';
+import { RootState } from '@/lib/redux-toolkit/store';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 export function EditNoteModal() {
+  const selectedNoteGlobalState = useSelector(
+    (state: RootState) => state.selectedNoteGlobalState,
+  );
+  const [newValue, setNewValue] = useState({
+    title: selectedNoteGlobalState.title,
+    content: selectedNoteGlobalState.content,
+  });
+
+  const handleUpdate = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const updatedData = {
+        title: newValue.title,
+        content: newValue.content,
+      };
+      const { data: updatedNote } = await axios.put(
+        `/api/note/${selectedNoteGlobalState.id}`,
+        updatedData,
+      );
+    } catch (error) {}
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -23,7 +50,27 @@ export function EditNoteModal() {
         <DialogHeader>
           <DialogTitle>Edit Note?</DialogTitle>
           <DialogDescription>
-            <Input type="text" placeholder="note" />
+            <form onSubmit={handleUpdate}>
+              <Input
+                type="text"
+                placeholder="note"
+                value={newValue.title}
+                onChange={(e) =>
+                  setNewValue({ ...newValue, title: e.target.value })
+                }
+              />
+
+              <Textarea
+                placeholder="note description"
+                value={newValue.content}
+                onChange={(e) =>
+                  setNewValue({ ...newValue, content: e.target.value })
+                }
+              />
+              <div>
+                <Button type="submit">Update</Button>
+              </div>
+            </form>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
